5f1a7893ff4f640390a509af669e48ea57fe619b
Marius <marius@192.168.2.26>
2016-11-15 11:25:08 +0100

    added ALL the things

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'arcsightrest',
+  packages = ['arcsightrest'],
+  version = '1.0',
+  description = 'Python library to connect to the HP Arcsight Logger REST API',
+  author = 'Marius Iversen',
+  author_email = 'marius@chasenet.org',
+  url = 'https://github.com/P1llus/ArcSight-Rest',
+  download_url = 'https://github.com/P1llus/ArcSight-Rest/tarball/1.0',
+  keywords = ['arcsight', 'logger', 'rest'],
+  classifiers = [],
+)
